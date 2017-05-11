103d2b49499be8b20fe8d88c0fcabcbcdcd32f6d
Jeremy Barzas <jbarzas@gmail.com>
2017-05-11 11:54:18 -0500

    character container inventory
    added the character, container, inventory monobehaviours and
    scriptableobjects

---
+++ Assets/Scripts/ContainerConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ContainerConfig", menuName = "ContainerConfig")]
+
+public class ContainerConfig : ScriptableObject
+{
+    public List<Item> contents;
+}
---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public InventoryConfig inventoryConfig;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
