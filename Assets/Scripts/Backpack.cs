50388cf6c64cff281c2205e6d864d8664c7b7ba2
Jeremy Barzas <jbarzas@gmail.com>
2017-05-11 16:18:39 -0500

    updated item character and backpack scripts

---
+++ Assets/Scripts/Backpack.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Backpack : MonoBehaviour
+{
+    public BackpackConfig backpackConfig;
+    public List<Item> contents;
+    public float maxWeight;
+    private float currentWeight;
+
+    // Use this for initialization
+    void Start ()
+    {
+        contents = backpackConfig.contents;
+        maxWeight = backpackConfig.maxWeight;
+	}
+
+    public bool AddItem(Item item)
+    {
+        if (currentWeight + item.weight < maxWeight)
+        {
+            contents.Add(item);
+            return true;
+        }
+        return false;
+    }
+    public bool RemoveItem(Item item)
+    {
+        int count = contents.Count;
+        contents.Remove(item);
+        if (contents.Count < count)
+        {
+            return true;
+        }
+        return false;
+    }
+}
