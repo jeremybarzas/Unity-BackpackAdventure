7302ec990f55ae1b581f292eed5fc0880a603a82
Jeremy Barzas <jbarzas@gmail.com>
2017-05-12 10:24:43 -0500

    added seperate potion classes

--- Assets/HealthPotion.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Item/HealthPotion")]
-public class HealthPotion : Item {
-    
-}
---
+++ Assets/Scripts/Scriptable/HealthPotion.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Item/Potion/HealthPotion")]
+
+public class HealthPotion : Potion
+{
+    public string itemName;
+    public string itemID;
+    public int healthAmount;
+}
