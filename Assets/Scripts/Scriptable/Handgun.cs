5ff36019bad7d8992c3a85517de4f5716fda3765
Conner-Stack <connerstack@gmail.com>
2017-05-12 12:01:29 -0500

    Equipment inheritence initialization push
    created all classes inheriting from the Equipment inheritance of Item
    class

---
+++ Assets/Scripts/Scriptable/Handgun.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Item/Equipment/Weapon/Handgun")]
+
+public class Handgun : Weapon
+{
+    new public string itemName;
+    new public string itemID;
+    public int atkDmg;
+}
