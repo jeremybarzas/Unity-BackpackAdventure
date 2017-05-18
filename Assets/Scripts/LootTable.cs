1c928b7e80bfdc3d1497e7ed1949f2bd28a2ce9a
Conner-Stack <connerstack@gmail.com>
2017-05-18 12:45:00 -0500

    push
    setup for loot table

---
+++ Assets/Scripts/LootTable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+[CreateAssetMenu(menuName ="Loot Table")]
+public class LootTable : ScriptableObject {
+     
+  
+    [System.Serializable]
+    public class ItemDrop
+    {
+        public Item droppedItem;
+        public float dropChance = UnityEngine.Random.Range(0f, 1f);
+
+}
+    public float randomRoll = UnityEngine.Random.Range(0f, 1f);
+    public List<ItemDrop> Drops;
+    List<ItemDrop> GetDrops()
+    {
+        foreach
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
