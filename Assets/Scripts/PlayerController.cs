a7e5b1014a8de98527c580048d87b7c78b5c24d5
stackconner <connerstack@gmail.com>
2017-05-24 18:04:05 -0500

    push
    sprint push

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,10 +13,16 @@ public class PlayerController : MonoBehaviour {
 
     void CharacterMove()
     {
+        float speed = 3.0f;
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = 12.0f;
+        }
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
+
     }
     void FixedUpdate()
     {
