6756f17f33551851e4beca0b493074c015e729c0
Jeremy Barzas <jbarzas@gmail.com>
2017-06-01 16:31:49 -0500

    rename and reorder of compnents in inspector

--- Assets/Scripts/Scriptable/LongSword.cs
+++ Assets/Scripts/Scriptable/LongSword.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-[CreateAssetMenu(menuName = "Weapon/LongSword")]
-public class LongSword : Weapon
+[CreateAssetMenu(menuName = "Weapon/Longsword")]
+public class Longsword : Weapon
 {
 }
